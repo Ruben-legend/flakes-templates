ce039b569666386c33c85dddfa1308486b6bf08f
Ruben-legend <canojimenezrubenalexander@gmail.com>
2025-10-24 10:47:49 -0400

    fix: name error

--- flake.nix
+++ flake.nix
@@ -2,15 +2,19 @@
   description = "A Collection of personal nix flake template";
 
   output = {self, ...}:{
-    template = {
+    templates = {
       java = {
         path = ./java;
         description = "This is a template of java";
       };
+
       rust = {
         path = ./rust;
         description = "this is a template of rust";
       };
+
     };
+
   };
+
 }

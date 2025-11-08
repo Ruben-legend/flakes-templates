541acd73b6c8c94d524b6238ad23695dfe62b40c
Ruben Alexander <103773863+myticalruben@users.noreply.github.com>
2025-11-08 09:48:42 -0400

    Add Node flake template to flake.nix

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
         path = ./templates/java;
         description = "Java flake template.";
       };
+      node = {
+        path = ./templates/node;
+        description = "Node flake template";
+      };
       rust = {
         path = ./templates/rust;
         description = "Rust flake template.";
